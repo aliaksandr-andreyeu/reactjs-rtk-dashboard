e55ce47c8ff1aace998589c92379a1e2c9c842f3
Aliaksandr Andreyeu <andreyeu.aliaksandr@gmail.com>
2023-02-09 12:18:49 +0300

    Add button, input components

---
+++ src/components/button/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './styles.scss';
+
+const Button = ({ disabled, type, label, className, onClick }) => {
+  const getClassName = (...args) => {
+    return Array.from(args).filter(Boolean).join(' ');
+  };
+
+  return (
+    Boolean(label) && (
+      <button className={getClassName('cmp-button', className)} type={type} disabled={disabled} onClick={onClick}>
+        {label}
+      </button>
+    )
+  );
+};
+
+Button.propTypes = {
+  className: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func
+};
+
+Button.defaultProps = {
+  className: '',
+  type: 'button',
+  disabled: false,
+  onClick: (event) => event
+};
+
+export default Button;
---
+++ src/components/input/index.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import './styles.scss';
+
+const Input = ({
+  label,
+  type,
+  inputClassName,
+  labelClassName,
+  containerClassName,
+  autoFocus,
+  tabIndex,
+  disabled,
+  readonly,
+  placeholder,
+  value,
+  onChange,
+  name
+}) => {
+  const [val, setVal] = useState(value !== undefined ? value : '');
+
+  const getClassName = (...args) => {
+    return Array.from(args).filter(Boolean).join(' ');
+  };
+
+  const handleChange = (event) => {
+    setVal(event.target.value);
+    Boolean(onChange) && onChange(event);
+  };
+
+  return (
+    <div className={getClassName('cmp-input-box', containerClassName)}>
+      {Boolean(label) && <label className={getClassName('cmp-input-label', labelClassName)}>{label}</label>}
+      <input
+        className={getClassName('cmp-input', inputClassName)}
+        autoFocus={autoFocus}
+        tabIndex={tabIndex}
+        disabled={disabled}
+        readOnly={readonly}
+        type={type}
+        placeholder={placeholder}
+        value={val}
+        onChange={handleChange}
+        {...(name && { name })}
+      />
+    </div>
+  );
+};
+
+Input.propTypes = {
+  label: PropTypes.string,
+  name: PropTypes.string,
+  type: PropTypes.string,
+  inputClassName: PropTypes.string,
+  labelClassName: PropTypes.string,
+  containerClassName: PropTypes.string,
+  autoFocus: PropTypes.bool,
+  tabIndex: PropTypes.number,
+  disabled: PropTypes.bool,
+  readonly: PropTypes.bool,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func
+};
+
+Input.defaultProps = {
+  type: 'text',
+  autoFocus: false,
+  tabIndex: 0,
+  disabled: false,
+  readonly: false,
+  value: '',
+  label: '',
+  name: '',
+  inputClassName: '',
+  labelClassName: '',
+  containerClassName: '',
+  placeholder: '',
+  onChange: (event) => event
+};
+
+export default Input;
