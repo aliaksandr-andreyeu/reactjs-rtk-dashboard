e141591307b7e92826bfad6e2a04ee6c456a1429
Aliaksandr Andreyeu <andreyeu.aliaksandr@gmail.com>
2023-03-13 10:51:31 +0300

    Add forms input validation errors

---
+++ src/constants/errors/index.js
@@ -0,0 +1,16 @@
+const errors = {
+  validation: {
+    fieldRequired: 'This field is required',
+    emailRequired: 'E-mail is required',
+    emailInvalid: 'Invalid e-mail address',
+    passwordRequired: 'Password is required',
+    passwordIncorrect: 'Incorrect password',
+    confirmRequired: 'Password confirmation is required',
+    confirmIncorrect: 'Incorrect password confirmation'
+  },
+  common: {
+    error: 'Something went wrong. Try again later...'
+  }
+};
+
+export default errors;
