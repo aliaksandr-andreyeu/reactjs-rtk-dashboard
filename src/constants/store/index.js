bb8bd860b4cc5492e0405291b725fd57afa6387b
Aliaksandr Andreyeu <andreyeu.aliaksandr@gmail.com>
2023-03-24 13:13:46 +0300

    Add API methods for Users store slice

--- src/constants/store/index.js
+++ src/constants/store/index.js
@@ -6,7 +6,10 @@ const getResponseData = (response) => (Boolean(response) && response.data ? resp
 
 const requests = {
   get: (url) => axios.get(url).then((response) => getResponseData(response)),
-  post: (url, body = {}) => axios.post(url, body).then((response) => getResponseData(response))
+  post: (url, body = {}) => axios.post(url, body).then((response) => getResponseData(response)),
+  put: (url, body = {}) => axios.put(url, body).then((response) => getResponseData(response)),
+  patch: (url, body = {}) => axios.patch(url, body).then((response) => getResponseData(response)),
+  delete: (url) => axios.delete(url).then((response) => getResponseData(response))
 };
 
 export default requests;
--- src/store/api/users/index.js
+++ src/store/api/users/index.js
@@ -3,6 +3,21 @@ import { requests } from '@constants';
 const users = Object.create(Object.prototype, {
   getUsers: {
     value: () => requests.get(`${REACT_ENV.API_URL}/users`)
+  },
+  getUser: {
+    value: (id) => requests.get(`${REACT_ENV.API_URL}/users?id=${id}`)
+  },
+  createUser: {
+    value: (payload) => requests.post(`${REACT_ENV.API_URL}/users`, payload)
+  },
+  modifyUser: {
+    value: (payload) => requests.put(`${REACT_ENV.API_URL}/users`, payload)
+  },
+  updateUser: {
+    value: (payload) => requests.patch(`${REACT_ENV.API_URL}/users`, payload)
+  },
+  deleteUser: {
+    value: (id) => requests.delete(`${REACT_ENV.API_URL}/users?id=${id}`)
   }
 });
 
