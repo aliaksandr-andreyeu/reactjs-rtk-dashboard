8735135ec6382799fd05a06b26562f1523e304d3
Aliaksandr Andreyeu <andreyeu.aliaksandr@gmail.com>
2023-03-26 09:41:19 +0300

    Add about screen boilerplate content

--- src/containers/about/screen/index.js
+++ src/containers/about/screen/index.js
@@ -1,9 +1,59 @@
 import React from 'react';
 
-import './styles.scss';
-
 const AboutScreen = () => {
-  return <div>About</div>;
+  return (
+    <div className='data-container'>
+      <h1>About</h1>
+      <p>Hello, Dear friends, Welcome to Company also, we are happy you want to know something more about our site.</p>
+      <p>
+        So, basically, nowadays people are more dependent on online products and services that&apos;s why we also, take
+        forward a step to help you.
+      </p>
+      <p>
+        Our first wish is to provide you with a better solution to solve your problem. So, kindly if you don&apos;t get
+        any solution then mention it in the comment section.
+      </p>
+      <p>
+        Also, we are trying to provide fresh & latest content that provides you ideas about all updated information
+        that&apos;s happening in the world.
+      </p>
+      <p>In the below section you can get more ideas about our site like our website category and content category.</p>
+      <p>
+        If you have additional questions or require more information about our About Page, do not hesitate to contact us
+        through email at <b>company@email.com</b>
+      </p>
+      <h2>What is Our Goal?</h2>
+      <p>There are millions of websites created every day, also, there is much fake content spread on the internet.</p>
+      <p>
+        So, Our main goal is to provide you with 100% Original and Safe content that provides you a great and better
+        experience on the world wide web.
+      </p>
+      <p>
+        We mainly focus on our service so and improving it regularly to provide a better user experience to all users.
+      </p>
+      <p>
+        Basically, we focus on the Software Engineering niche so, our main priority is to search for new content and
+        present it in front of you to learn something new.
+      </p>
+      <h3>What is our Service?</h3>
+      <p>
+        We are mainly focused on the Software Engineering category so, we provide Software Engineering related content
+        if you are interested in the Software Engineering category then you can visit daily to get more latest
+        information.
+      </p>
+      <h3>Software Engineering</h3>
+      <p>
+        On our website Company you get can all Software Engineering related information also, we focus on many other
+        categories and we hope you like also, the content of other categories that are maintained on our website. So,
+        you can visit our website homepage to know all category details here you can visit our homepage.
+      </p>
+      <h3>About Company</h3>
+      <p>
+        As you can see already we mention what is our goal and Service again we repeat that we mainly focus on Software
+        Engineering Category to help people.
+      </p>
+    </div>
+  );
 };
 
 export default AboutScreen;
