a230dbe2f92fd92738c9ff97c9a22467be3a2e3c
Aliaksandr Andreyeu <andreyeu.aliaksandr@gmail.com>
2023-04-03 11:41:03 +0300

    Add contactUs && profile forms with responsive

--- src/containers/profile/screen/index.js
+++ src/containers/profile/screen/index.js
@@ -17,7 +17,7 @@ const ProfileScreen = ({
   changePassMessage,
   editAcc,
   editAccLoading,
-  editAccMessage,
+  editAccMessage
 }) => {
   const { age: oAge, job: oJob, name: oName, surname: oSurname, username: oUserName } = overview;
 
@@ -99,72 +99,96 @@ const ProfileScreen = ({
 
   return (
     <Fragment>
-      <div className='data-container'>
+      <div className='data-container profile'>
         <form className='data-form' onSubmit={handleSubmit}>
           <Alert className='alert-box' message={editAccMessage} type='success' />
           <Alert className='alert-box' message={changePassMessage} type='success' />
-          <Input
-            label={'Username'}
-            type={'text'}
-            containerClassName={'input-box'}
-            placeholder={'Username'}
-            name={'username'}
-            onChange={checkEmail}
-            error={userNameError}
-            value={userName}
-            disabled={!edit}
-          />
-          <a className='a-link' onClick={handleChangePassword}>
-            Change Password
-          </a>
-          <Input
-            label={'Name'}
-            type={'text'}
-            containerClassName={'input-box'}
-            placeholder={'Name'}
-            name={'name'}
-            onChange={setName}
-            value={name}
-            disabled={!edit}
-          />
-          <Input
-            label={'Surname'}
-            type={'text'}
-            containerClassName={'input-box'}
-            placeholder={'Surname'}
-            name={'surname'}
-            onChange={setSurname}
-            value={surname}
-            disabled={!edit}
-          />
-          <Input
-            label={'Age'}
-            type={'number'}
-            containerClassName={'input-box'}
-            placeholder={'Age'}
-            name={'age'}
-            onChange={setAge}
-            value={age || ''}
-            disabled={!edit}
-          />
-          <Input
-            label={'Job'}
-            type={'text'}
-            containerClassName={'input-box'}
-            placeholder={'Job'}
-            name={'job'}
-            onChange={setJob}
-            value={job}
-            disabled={!edit}
-          />
-          <Button
-            loading={editAccLoading}
-            color={'accent'}
-            label={edit ? 'Save' : 'Edit'}
-            type={'submit'}
-            className={'btn'}
-            onClick={handleSubmit}
-          />
+          <div className='row'>
+            <div className='col'>
+              <div className='cell'>
+                <Input
+                  label={'Username'}
+                  type={'text'}
+                  containerClassName={'input-box'}
+                  placeholder={'Username'}
+                  name={'username'}
+                  onChange={checkEmail}
+                  error={userNameError}
+                  value={userName}
+                  disabled={!edit}
+                />
+              </div>
+              <div className='cell'>
+                <Input
+                  label={'Name'}
+                  type={'text'}
+                  containerClassName={'input-box'}
+                  placeholder={'Name'}
+                  name={'name'}
+                  onChange={setName}
+                  value={name}
+                  disabled={!edit}
+                />
+              </div>
+              <div className='cell'>
+                <Input
+                  label={'Surname'}
+                  type={'text'}
+                  containerClassName={'input-box'}
+                  placeholder={'Surname'}
+                  name={'surname'}
+                  onChange={setSurname}
+                  value={surname}
+                  disabled={!edit}
+                />
+              </div>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col'>
+              <div className='cell'>
+                <Input
+                  label={'Age'}
+                  type={'number'}
+                  containerClassName={'input-box'}
+                  placeholder={'Age'}
+                  name={'age'}
+                  onChange={setAge}
+                  value={age || ''}
+                  disabled={!edit}
+                />
+              </div>
+              <div className='cell'>
+                <Input
+                  label={'Job'}
+                  type={'text'}
+                  containerClassName={'input-box'}
+                  placeholder={'Job'}
+                  name={'job'}
+                  onChange={setJob}
+                  value={job}
+                  disabled={!edit}
+                />
+              </div>
+              <div className='cell justify-center'>
+                <a className='a-link' onClick={handleChangePassword}>
+                  Change Password
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col items-center'>
+              <Button
+                loading={editAccLoading}
+                color={edit ? 'accent' : 'primary'}
+                label={edit ? 'Save' : 'Edit'}
+                type={'submit'}
+                className={'btn'}
+                onClick={handleSubmit}
+              />
+            </div>
+          </div>
         </form>
       </div>
       <ChangePasswordModal
@@ -186,7 +210,7 @@ ProfileScreen.propTypes = {
   changePassMessage: PropTypes.oneOfType([PropTypes.string, PropTypes.oneOf([null])]),
   editAcc: PropTypes.func.isRequired,
   editAccLoading: PropTypes.bool,
-  editAccMessage: PropTypes.oneOfType([PropTypes.string, PropTypes.oneOf([null])]),
+  editAccMessage: PropTypes.oneOfType([PropTypes.string, PropTypes.oneOf([null])])
 };
 
 ProfileScreen.defaultProps = {
@@ -197,7 +221,7 @@ ProfileScreen.defaultProps = {
   changePassMessage: null,
   editAcc: (payload) => payload,
   editAccLoading: false,
-  editAccMessage: null,
+  editAccMessage: null
 };
 
 export default ProfileScreen;
